395d2160a0a06d404c1db1a627f909b48a7d1c91
Matthew Robertson <matt.robertson904@gmail.com>
2021-11-06 16:10:00 -0400

    reverted avif files to jpg

--- contactus.php
+++ contactus.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 var_dump($_GET);
-  $cmd = 'echo "'.$_GET["name"]." : ".$_GET["phone"]." : ".$_GET["msg"]." : ".$_GET["email"].'"'. " | sendmail kwiley@gentechsolutions.com";
+  $cmd = 'echo "'.$_GET["name"]." : ".$_GET["phone"]." : ".$_GET["msg"]." : ".$_GET["email"].'"'. " | sendmail contact@gentechsolutions.com";
   $res = exec($cmd);
   header('Location: https://gentechsolutions.com/?submitted=ok');
 ?>
