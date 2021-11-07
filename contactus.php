815e2e3958491cbe5c43246148455040da4ab2de
Matthew Robertson <matt.robertson904@gmail.com>
2021-11-07 15:55:46 -0500

    contact from reversion

--- contactus.php
+++ contactus.php
@@ -3,7 +3,9 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 var_dump($_GET);
-  $cmd = 'echo "'.$_GET["name"]." : ".$_GET["phone"]." : ".$_GET["msg"]." : ".$_GET["email"].'"'. " | sendmail contact@gentechsolutions.com";
+  $cmd = 'echo "'.$_GET["name"]." : ".$_GET["phone"]." : ".$_GET["msg"]." : ".$_GET["email"].'"'. " | sendmail kwiley@gentechsolutions.com";
+  // $cmd2 = 'echo "'.$_GET["name"]." : ".$_GET["phone"]." : ".$_GET["msg"]." : ".$_GET["email"].'"'. " | sendmail contact@gentechsolutions.com";
   $res = exec($cmd);
+  // $res2 = exec($cmd2);
   header('Location: https://gentechsolutions.com/?submitted=ok');
 ?>
